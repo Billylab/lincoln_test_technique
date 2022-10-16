34013560be2b715c4dd3608a3a3bd24f7170ad83
Billylab <billytran75013@gmail.com>
2022-10-16 13:05:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo Starting Python script : 0_landing_to_raw
-python pipeline/0_landing_to_raw/0_landing_to_raw.py
+python pipeline/0_landing_to_raw/manual_files/0_landing_to_raw.py
 
 echo Starting Python script : 1_make_json_graph
 python pipeline/1_make_json_graph/1_make_json_graph.py
+
+
